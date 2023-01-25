705d334757b4dd774ab50dfabbdf61088a45fa56
hfrSchmidt <9467795+hfrSchmidt@users.noreply.github.com>
2023-01-25 10:17:44 +0000

    Add cookiecutter variables
    Also added the variables in setup.py and pyproject.toml.

--- {{ cookiecutter.repository_name }}/setup.py
+++ {{ cookiecutter.repository_name }}/setup.py
@@ -5,8 +5,15 @@ import os
 
 import setuptools
 
-dependencies = ["python-freeipa >= 1.0"]
+dependencies = [{%- if cookiecutter.command_line_interface | lower == 'click'%}'Click>=8.0',{%- endif%}]
 
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
 
 # Setup boilerplate below this line.
 
@@ -20,17 +27,16 @@ history_filename = os.path.join(package_root, "HISTORY.rst")
 with io.open(history_filename, encoding="utf-8") as history_file:
     history = history_file.read()
 
-# Only include packages under the 'infra' namespace. Do not include tests,
+# Only include packages under the '{{ cookiecutter.package_name }}' namespace. Do not include tests,
 # benchmarks, etc.
 packages = [
-    package for package in setuptools.find_packages() if package.startswith("infra")
+    package for package in setuptools.find_packages() if package.startswith("{{ cookiecutter.package_name }}")
 ]
 
-# Determine which namespaces are needed.
-namespaces = ["infra"]
-if "infra.usermgmt" in packages:
-    namespaces.append("infra.usermgmt")
-
+# Determine which namespaces are needed, when namespaced packages are supposed to be used
+# namespaces = ["{{ cookiecutter.package_name }}"]
+# if "{{ cookiecutter.package_name }}.<namespace>" in packages:
+#     namespaces.append("{{ cookiecutter.packe_name }}.<namespace>")
 
 setuptools.setup(
     name="{{ cookiecutter.repository_name }}",
@@ -43,21 +49,34 @@ setuptools.setup(
     url="https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repository_name }}",
     classifiers=[
         "{{ cookiecutter.release_status }}",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
+{%- if cookiecutter.open_source_license in license_classifiers %}
+        "{{ license_classifiers[cookiecutter.open_source_license] }}",
+{%- endif %}
+{%- for audience_item in coookiecutter.classifiers.audience %}
+        "{{ audience_item }}",
+{%- endfor %}
+{%- for os_item in coookiecutter.classifiers.os %}
+        "{{ os_item }}",
+{%- endfor %}
+{%- for topic_item in coookiecutter.classifiers.topic %}
+        "{{ topic_item }}",
+{%- endfor %}
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: System :: Systems Administration",
+{%- for py_version_item in coookiecutter.classifiers.python_versions %}
+        "Programming Language :: Python :: {{ py_version_item }}",
+{%- endfor %}
     ],
     packages=packages,
-    namespace_packages=namespaces,
+    # namespace_packages=namespaces,
     install_requires=dependencies,
-    python_requires=">=3.7",
+{%- if 'n' not in cookiecutter.command_line_interface|lower %}
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.package_name }}={{ cookiecutter.package_name }}.cli:main',
+        ],
+    },
+{%- endif %}
+    python_requires=">={{ cookiecutter.min_python_version }}",
     include_package_data=True,
     zip_safe=False,
 )
