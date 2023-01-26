c65f7de21851f59f805c853f468ee1d54da39fde
hfrSchmidt <9467795+hfrSchmidt@users.noreply.github.com>
2023-01-26 14:21:59 +0000

    Add cli stub

---
+++ {{ cookiecutter.repository_name }}/{{ cookiecutter.package_name }}/cli.py
@@ -0,0 +1,17 @@
+"""Console script for {{ cookiecutter.package_name }}."""
+
+{%- if cookiecutter.command_line_interface|lower == 'click' %}
+import click
+{%- endif %}
+
+{% if cookiecutter.command_line_interface|lower == 'click' %}
+@click.command()
+def main(args=None):
+    """Console script for {{ cookiecutter.package_name }}."""
+    click.echo("Place your code into "
+               "{{ cookiecutter.package_name }}.cli.main")
+    return 0
+{%- endif %}
+
+if __name__ == "__main__":
+    sys.exit(main())
