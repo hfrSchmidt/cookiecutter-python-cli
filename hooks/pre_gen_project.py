fe2d931d50cb20babb5046e2b6bc362d0b5653ed
hfrSchmidt <9467795+hfrSchmidt@users.noreply.github.com>
2023-01-27 11:22:42 +0000

    Fix broken variable name in pre gen

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,12 +2,12 @@ import re
 import sys
 
 
-MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+PACKAGE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.module_name }}'
+package_name = '{{ cookiecutter.package_name }}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: %s is not a valid Python module name!' % module_name)
+if not re.match(PACKAGE_REGEX, package_name):
+    print('ERROR: %s is not a valid Python module name!' % package_name)
 
     # exits with status 1 to indicate failure
     sys.exit(1)
